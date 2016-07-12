7c824d5b4a1003cd535f363212c59dcfde100292
Ryan Lim <rlim1812@gmail.com>
2016-07-11 23:55:11 -0700

    This is the first version of the app

--- Times Tables/ViewController.swift
+++ Times Tables/ViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        table.reloadData()
+    }
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return 20
+        
+    }
 
-
+    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        let timesTable = Int(sliderValue.value * 20)
+        cell.textLabel?.text = String(timesTable * (indexPath.row + 1))
+        return cell
+    }
+    
 }
 
